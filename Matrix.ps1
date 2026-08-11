dfcbd70dd80f46bd33cbd8757fbf066eb47e972e
James Brundage <+@users.noreply.github.com>
2026-08-11 11:34:50 -0700

    docs: `Matrix.ps1` docs ( Fixes #2 )

--- Matrix.ps1
+++ Matrix.ps1
@@ -130,16 +130,28 @@
 )]
 [CmdletBinding(PositionalBinding=$false)]
 param(
+# Any arguments for the transform function
+# Arguments can include numbers of CSS units.
+# `deg` and `turn` are converted into radians
+# `%` becomes a value between 0 and 1 
 [Parameter(ValueFromRemainingArguments)]
 [Alias('Arguments','Argument','Args')]
 [object[]]
 $ArgumentList,
 
+# Any input objects.
+# If the input object has a `Transform` static method,
+# it will be transformed.
+# If it does not, it will be passed thru.
 [Parameter(ValueFromPipeline)]
 [Alias('Input')]
 [PSObject[]]
 $InputObject,
 
+# The name of the method or property of a matrix transform.
+# If this is provided, this method will be called instead.
+# Many aliases, such as `Skew` or `Rotate`,
+# will use a custom member and will ignore this parameter.
 [ArgumentCompleter({
     param(
         $CommandName, $parameterName, $wordToComplete,
@@ -165,23 +177,28 @@ $InputObject,
 $Member = 'Create'
 )
 
+# Get our invocation name
 $myName = $MyInvocation.InvocationName
 
+# and all of our input
 $allInput = @($input)
 
-if (-not $allInput) {
+# If input was not piped
+if (-not $allInput.Length) {
+    # bind it to any unpiped input object
     $allInput += $InputObject
 }
 
+# Attempt to determine the matrix type.
 $matrixType = 
-    if ($myName -match '3d' -or $myName -match '4x4') {
-        [Numerics.Matrix4x4]
-    } else {
-        [Numerics.Matrix3x2]
+    # If the name contains 3d or 4x4,    
+    if ($myName -match '(?>3d|4x4)') {
+        [Numerics.Matrix4x4] # treat it as a 3d matrix.
+    } else { # Otherwise
+        [Numerics.Matrix3x2] # treat it as a 2d matrix.
     }
 
-
-# A quick little filter to convert css units into numbers.
+# Declare a  quick little filter to convert css units into numbers.
 filter cssunit {
     $arg = $_
     if ($arg -isnot [string]) {
@@ -211,50 +228,69 @@ filter cssunit {
     }
 }
 
-
+# Then convert all of our arguments into units
 $ArgumentList = @(
     $ArgumentList | cssunit    
 )
 
-$Matrix = $null
-
+# Next we will be determine the right matrix transform
+# Switch based off the name.
+# Any matching CSS transforms should be their literal equivalent.
+# These are _mostly_ self explanatory, with one _very_ annoying outlier
 switch ($myName) {
-    Rotate {        
+    Rotate {
+        # Rotate becomes `[Numerics.Matrix3x2]::CreateRotation`
         $MatrixType = [Numerics.Matrix3x2]
         $Member = 'CreateRotation'
         $ArgumentList = $ArgumentList[0]
     }
     RotateX {
+        # RotateX becomes `[Numerics.Matrix4x4]::CreateRotationX`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateRotationX'
         $ArgumentList = $ArgumentList[0]
     }
     RotateY {
+        # RotateY becomes `[Numerics.Matrix4x4]::CreateRotationY`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateRotationY'
         $ArgumentList = $ArgumentList[0]
     }
     RotateZ {
+        # RotateZ becomes `[Numerics.Matrix4x4]::CreateRotationZ`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateRotationZ'
         $ArgumentList = $ArgumentList[0]
     }
     Rotate3d {
-        
+        # Rotate3d is the complicated one.
+        # It took some digging, but the CSS working defines rotate3d in matrix form
         # https://drafts.csswg.org/css-transforms-2/#Rotate3dDefined
         
+        # It's a 4x4 matrix
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'Create'
+        # that requires normalized vectors
         $x, $y, $z, $null = $ArgumentList
+        # Sadly, this Normalize is different than .NET's Normalize
         $normalize = [Numerics.Vector3]::new($x, $y, $z)
+        # We need to get the sum of all squares (the length)
         $magnitude = $normalize.Length()
+        # If that sum was zero, make it the sqrt root of 3
         if ($magnitude -eq 0) { $magnitude = [Math]::Sqrt(3) }
+        # Normalize x y z with the magnitude
         $x, $y, $z = ($x/$magnitude), ($y/$magnitude), ($z/$magnitude)
+        # The formula requires each value squared, so do that now
         $x2, $y2, $z2 = [Math]::Pow($x, 2), [Math]::Pow($y,2), [Math]::Pow($z, 2)
+        # It also defines the angle as `alpha`
         $alpha = $ArgumentList[3] -as [single]
+        # And then uses this bit of trig to find a point in the unit circle 
         $sc = [Math]::Sin($alpha/2) * [Math]::Cos($alpha/2)
+        # and this to find the max size of the square, given that angle
         $sq = [Math]::Pow([Math]::Sin($alpha/2), 2)                
 
+        # This next bit of complexity is translated directly from the reference.
+        # With spacing and docs added for clarity.
         $ArgumentList = @(
             #M 1 1
             1 - (2 * ($y2 + $z2) * $sq)
@@ -264,7 +300,6 @@ switch ($myName) {
             2 * (($x * $z * $sq) + ($y * $sc))
             # M 1 4
             0
-
             
             # M 2 1
             2 * (($x * $y * $sq) + ($z * $sc))
@@ -299,32 +334,40 @@ switch ($myName) {
             # M 4 4
             1
         )
-        
+
+        # Every other CSS transform can be done in a few lines of PowerShell
+        # This particular transform is the (quite painful) outlier.
     }
     Scale {
+        # Scale becomes `[Numerics.Matrix3x2]::CreateScale`
         $MatrixType = [Numerics.Matrix3x2]
-        $Member = 'CreateScale'        
+        $Member = 'CreateScale'
     }
     ScaleX {
+        # `ScaleX` becomes `[Numerics.Matrix4x4]::CreateScale`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateScale'
         $ArgumentList = $ArgumentList[0], 1, 1
     }
     ScaleY {
+        # `ScaleY` becomes `[Numerics.Matrix4x4]::CreateScale`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateScale'
         $ArgumentList = 1, $ArgumentList[0], 1
     }
     ScaleZ {
+        # `ScaleZ` becomes `[Numerics.Matrix4x4]::CreateScale`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateScale'
         $ArgumentList = 1, 1, $ArgumentList[0]
     }
     Scale3d {
+        # `Scale3d` becomes `[Numerics.Matrix4x4]::CreateScale`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateScale'        
     }
     Skew {
+        # `Skew` becomes `[Numerics.Matrix3x2]::CreateSkew`
         $MatrixType = [Numerics.Matrix3x2]
         $Member = 'CreateSkew'
         if ($ArgumentList.Length -eq 1) {
@@ -332,6 +375,7 @@ switch ($myName) {
         }
     }
     SkewX {
+        # `SkewX` becomes `[Numerics.Matrix3x2]::CreateSkew`
         $MatrixType = [Numerics.Matrix3x2]
         $Member = 'CreateSkew'
         if ($ArgumentList.Length -eq 1) {
@@ -339,6 +383,7 @@ switch ($myName) {
         }
     }
     SkewY {
+        # `SkewY` becomes `[Numerics.Matrix3x2]::CreateSkew`
         $MatrixType = [Numerics.Matrix3x2]
         $Member = 'CreateSkew'
         if ($ArgumentList.Length -eq 1) {
@@ -346,71 +391,91 @@ switch ($myName) {
         }
     }
     Translate {
+        # `Translate` becomes `[Numerics.Matrix3x2]::CreateTranslation`
         $MatrixType = [Numerics.Matrix3x2]
         $Member = 'CreateTranslation'
     }
     Translate3d {
+        # `Translate3d` becomes `[Numerics.Matrix4x4]::CreateTranslation`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateTranslation'
     }    
     TranslateX {
+        # `TranslateX` becomes `[Numerics.Matrix4x4]::CreateTranslation`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateTranslation'
         $ArgumentList = @($ArgumentList[0], 0, 0)
     }
     TranslateY {
+        # `TranslateY` becomes `[Numerics.Matrix4x4]::CreateTranslation`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateTranslation'
         $ArgumentList = @(0, $ArgumentList[0], 0)
     }
     TranslateZ {
+        # `TranslateZ` becomes `[Numerics.Matrix4x4]::CreateTranslation`
         $MatrixType = [Numerics.Matrix4x4]
         $Member = 'CreateTranslation'
         $ArgumentList = @(0, 0, $ArgumentList[0])
     }
 }
 
+# If we do not have a matching member
 if ($null -eq $matrixType::$Member) {
+    # error out
+    Write-Error "$Member does not exist on $MatrixType"
     return
 }
 
+# If we have no arguments
 if (-not $ArgumentList.Length) {
+    # and no member, return the matrix type.
     if (-not $PSBoundParameters.Member) {return $matrixType } 
+    # If we have no arguments and a member, return the member.
     else { return $matrixType::$member }   
 }
 
+# If the first argument is a static member
 if ($ArgumentList.Length -eq 1 -and 
     $matrixType::($ArgumentList[0])
 ) {
-    $Member = 'identity'
-}
-
-if (-not $matrix) {
-    $matrix = 
-        if ($matrixType::$member.Invoke) {
-            $matrixType::$Member.Invoke($ArgumentList)
-        } else {
-            $matrixType::$Member
-        }
+    # use that as the member
+    $Member = $ArgumentList[0]
 }
 
 
+# Create the matrix by invoking the member
+# (or just returning the property)
+$matrix = 
+    if ($matrixType::$member.Invoke) {
+        $matrixType::$Member.Invoke($ArgumentList)
+    } else {
+        $matrixType::$Member
+    }
+
+# If we have any input
 if ($allInput.Length -and ($null -ne $allInput[0])) {
-    $3dMatrix  = 
-        if ($matrix -is [Numerics.Matrix3x2]) {
+    # Make a 3d variation of our matrix
+    $3dMatrix  =
+        if ($matrix -is [Numerics.Matrix3x2]) {        
             [Numerics.Matrix4x4]::Create($matrix)
         } else {
             $matrix
         }
 
+    # Walk over all of our input
     foreach ($in in $allInput) {
-        if ($in::Transform) {
+        # If the input is transformable
+        if ($in::Transform.Invoke) {
+            # transform the input.
             $in::Transform($in, $3dMatrix)
         } else {
+            # Otherwise, pass the input thru.
             $in
         }
     }
 } else {
+    # If we had no input, output the matrix.
     $matrix
 }
 
