7faa6756635e1b75fe651c47de2e1cd09495a093
James Brundage <+@users.noreply.github.com>
2026-08-19 11:52:14 -0700

    feat: `config.ps1` ( Fixes #23 )

---
+++ config.ps1
@@ -0,0 +1,10 @@
+# Aliasing /includes.
+Set-Alias /_includes/CopyCode ./_includes/CopyCode.ps1
+Set-Alias /_includes/Palette ./_includes/Palette.ps1
+Set-Alias /_includes/FeatherIcon ./_includes/FeatherIcon.ps1
+
+# Alias files we will use inline
+Set-Alias Matrix.css ./Matrix.css.ps1
+Set-Alias Matrix.svg ./Matrix.svg.ps1
+Set-Alias /Matrix.css ./Matrix.css.ps1
+Set-Alias /Matrix.svg ./Matrix.svg.ps1
