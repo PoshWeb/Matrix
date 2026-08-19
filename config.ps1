d3cdc9fde697f9a79dfa0bdba1e9f29340161049
James Brundage <+@users.noreply.github.com>
2026-08-19 16:02:47 -0700

    feat(website): `/_includes/Menu` ( Fixes #29 )

--- config.ps1
+++ config.ps1
@@ -3,6 +3,7 @@ Set-Alias /_includes/CopyCode ./_includes/CopyCode.ps1
 Set-Alias /_includes/Palette ./_includes/Palette.ps1
 Set-Alias /_includes/FeatherIcon ./_includes/FeatherIcon.ps1
 Set-Alias /_includes/Sitemap ./_includes/Sitemap.ps1
+Set-Alias /_includes/Menu ./_includes/Menu.ps1
 
 # Alias files we will use inline
 Set-Alias Matrix.css ./Matrix.css.ps1
