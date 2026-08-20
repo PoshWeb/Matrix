a6a114d6b424602f1c9a2788b8c2c5e63e66e90a
James Brundage <+@users.noreply.github.com>
2026-08-20 13:58:01 -0700

    feat: `Matrix3x2.MathML` ( Fixes #33 )

---
+++ Types/Matrix3x2/get_MathML.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+    Matrix MathML
+.DESCRIPTION
+    Gets the Matrix as a MathML representation of itself.    
+#>
+[xml]@"
+<math display='block'>
+    <mo>(</mo>
+    <mtable>
+    $(
+        foreach ($row in 1..3) {
+            "<mtr>"
+            foreach ($col in 1..2) {
+                "<mtd><mn>$($this."M${row}${col}")</mn></mtd>"
+            }
+            "</mtr>"
+        }
+    )
+    </mtable>
+    <mo>)</mo>  
+</math>
+"@
