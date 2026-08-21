be48f1d65222817bfdaa24e6f2f72517c184f052
James Brundage <+@users.noreply.github.com>
2026-08-21 10:05:03 -0700

    feat: Quaternion.get_MathML` ( Fixes #37 )

---
+++ Types/Quaternion/get_MathML.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+    Matrix MathML
+.DESCRIPTION
+    Gets the Matrix as a MathML representation of itself.
+.NOTES
+    Also shows the equivalent `[Numerics.Matrtix4x4]`
+#>
+param()
+[xml]@"
+<math display='block'>
+<mo>[</mo>
+<mtable>
+$(
+    "<mtr>"
+    foreach ($var in 'X','Y','Z','W') {
+        "<mtd>"
+            "<mi>$var</mi>"
+            "<mo>=</mo>"
+            "<mn>$($this.$var)</mn>"        
+        "</mtd>"
+    }
+    "</mtr>"
+)
+</mtable>
+<mo>]</mo>
+<mo>=</mo>$(
+    [Numerics.Matrix4x4]::CreateFromQuaternion($this).MathML.math.innerXml
+)
+</math>
+"@
+
